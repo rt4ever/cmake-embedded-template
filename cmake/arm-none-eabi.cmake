a75c94eca27f38dff7698c6d0f0332e687b93757
homelab <bochao.me@foxmail.com>
2025-12-17 07:54:06 +0800

    更新 'cmake/arm-none-eabi.cmake'

--- cmake/arm-none-eabi.cmake
+++ cmake/arm-none-eabi.cmake
@@ -3,7 +3,6 @@ set(CMAKE_SYSTEM_PROCESSOR arm)
 
 # 强制指定编译器 (假设已添加到环境变量，否则写绝对路径)
 set(CMAKE_C_COMPILER "arm-none-eabi-gcc")
-set(CMAKE_CXX_COMPILER "arm-none-eabi-g++")
 set(CMAKE_ASM_COMPILER "arm-none-eabi-gcc")
 
 # [关键] 告诉 CMake 这是一个裸机工程，不需要链接系统库测试
